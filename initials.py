cf8a2316e1c58f660ae3bb4482998de6a643d466
2425402513 - Edzeani Daniel Edem <alexdave880@gmail.com>
2026-05-31 04:52:47 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Printing out my Initials
+# Assignment Number: 2
+#
+# Name: Daniel Edem Edzeani
+# STUDENT ID:  2425402513
+# Email: alexdave880@gmail.com
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Daniel Edem Edzeani, this programming assignment is my own work
+# and I have not provided this code to any other student
 
-def main():
-  pass #code goes here
+print(" ")  #Leaving a blank line
+
+print("...DEE")     #Declearing my Initials
+
+print(" ")
+def main():     #main function
+    #Starting with the Program
+  print("...DDDDDDDD............EEEEEEEEEEEE............EEEEEEEEEEEE...")
+  print("...DDDDDDDDDD..........EE......................EE.............")
+  print("...DD.......DDD........EE......................EE.............")
+  print("...DD........DD........EE......................EE.............")
+  print("...DD........DD........EEEEEEEEEEEE............EEEEEEEEEEEE...")
+  print("...DD........DD........EEEEEEEEEEEE............EEEEEEEEEEEE...")
+  print("...DD........DD........EE......................EE.............")
+  print("...DD.......DDD........EE......................EE.............")
+  print("...DDDDDDDDDD....**....EE...............**.....EE...........**")
+  print("...DDDDDDDD......**....EEEEEEEEEEEE.....**.....EEEEEEEEEEEE.**")
 
 
-
-main()
+if __name__ == "__main__":
+    main()
